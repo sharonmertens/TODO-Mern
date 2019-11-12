dc1cce5fe572c6bd2782c5aa96e254b64326bd08
Sharon Mertens <sharonmertens@Sharons-MacBook-Air-2.local>
2019-11-12 14:02:11 -0500

    implementing create todo component

--- mern-todo-app/src/components/create-todo.component.js
+++ mern-todo-app/src/components/create-todo.component.js
@@ -1,10 +1,92 @@
 import React, { Component } from 'react';
 
 export default class CreateTodo extends Component {
+
+  // add constructor to the component class:
+  constructor(props) {
+    super(props);
+
+    // because we are dealing with the component's state object, we need to make sure to bind the methods to this by adding:
+    this.onChangeTodoDescription = this.onChangeTodoDescription.bind(this);
+    this.onChangeTodoResponsible = this.onChangeTodoResponsible.bind(this);
+    this.onChangeTodoPriority = this.onChangeTodoPriority.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+
+    // inside constructor, setting the initial state of the component by assigning an object to this.state
+    this.state = {
+      todo_description: '',
+      todo_responsible: '',
+      todo_priority: '',
+      todo_completed: false
+    }
+  }
+
+  // adding methods which can be used to update the state properties
+  onChangeTodoDescription(e) {
+    this.setState({
+      todo_description: e.target.value
+    });
+  }
+
+  onChangeTodoResponsible(e) {
+    this.setstate({
+      todo_responsible: e.target.value
+    });
+  }
+
+  onChangeTodoPriority(e) {
+    this.setState({
+      todo_priority: e.target.value
+    });
+  }
+
+  // another method to handle the submit even of the form to create a new todo item
+
+onSubmit(e) {
+  // ensure that the default HTML form submit behavior is prevented
+  e.preventDefault();
+
+  console.log(`Form submitted:`);
+  console.log(`Todo Description: ${this.state.todo_description}`);
+  console.log(`Todo Responsible: ${this.state.todo_responsible}`);
+  console.log(`Todo Priority: ${this.state.todo_priority}`);
+
+  // resetting the form
+  this.setState({
+    todo_description: '',
+    todo_responsible: '',
+    todo_priority: '',
+    todo_completed: false
+  })
+}
+
+// add the JSK code  needed to display the form:
   render() {
     return (
-      <div>
-        <p>Welcome to Create Todo Component!!</p>
+      <div style={{marginTop: 10}}>
+        <h3>Create New Todo</h3>
+        <form onSubmit={this.onSubmit}>
+
+          <div className="form-group">
+            <label>Description: </label>
+            <input  type="text"
+                    className="form-control"
+                    value={this.state.todo_description}
+                    onChange={this.onChangeTodoDescription}
+                    />
+          </div>
+
+          <div className="form-group">
+            <label>Responsible: </label>
+            <input  type="text"
+                    className="form-control"
+                    value={this.state.todo_responsible}
+                    onChange={this.onChangeTodoResponsible}
+                    />
+          </div>
+
+          
+        </form>
       </div>
     )
   }
