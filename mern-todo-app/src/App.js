2270eb4d87e836bae39dca80282d70def4b6ad9d
Sharon Mertens <sharonmertens@Sharons-MacBook-Air-2.local>
2019-11-12 13:10:34 -0500

    small edits

--- mern-todo-app/src/App.js
+++ mern-todo-app/src/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 import "bootstrap/dist/css/bootstrap.min.css";
+
+import CreateTodo from "./components/create-todo.component";
+import EditTodo from "./components/edit-todo.component";
+import TodosList from "./components/todos-list.component";
 
 class App extends Component {
   render() {
