73c720a49089f88493c277f1506b3dcc098dbb4d
Sharon Mertens <sharonmertens@Sharons-MacBook-Air-2.local>
2019-11-12 12:36:33 -0500

    set up react router

--- mern-todo-app/src/App.js
+++ mern-todo-app/src/App.js
@@ -1,12 +1,18 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="container">
-        <h2>MERN-Stack Todo App</h2>
-      </div>
+      <Router>
+        <div className="container">
+          <h2>MERN-Stack Todo App</h2>
+          <Route path="/" exact component={TodosList} />
+          <Route path="/edit/:id" component={EditTodo} />
+          <Route path="/create" component={CreateTodo} />
+        </div>
+      </Router>
     );
   }
 }
