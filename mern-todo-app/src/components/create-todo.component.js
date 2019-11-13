fbca6b30207b6fd1d8c469b5d22c794158a9cdf9
Sharon Mertens <sharonmertens@Sharons-MacBook-Air-2.local>
2019-11-13 10:46:52 -0500

    implemented axios to create todo

--- mern-todo-app/src/components/create-todo.component.js
+++ mern-todo-app/src/components/create-todo.component.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class CreateTodo extends Component {
 
@@ -29,7 +30,7 @@ export default class CreateTodo extends Component {
   }
 
   onChangeTodoResponsible(e) {
-    this.setstate({
+    this.setState({
       todo_responsible: e.target.value
     });
   }
@@ -50,6 +51,16 @@ onSubmit(e) {
   console.log(`Todo Description: ${this.state.todo_description}`);
   console.log(`Todo Responsible: ${this.state.todo_responsible}`);
   console.log(`Todo Priority: ${this.state.todo_priority}`);
+
+  const newTodo = {
+    todo_description: this.state.todo_description,
+    todo_responsible: this.state.todo_responsible,
+    todo_priority: this.state.todo_priority,
+    todo_completed: this.state.todo_completed
+  };
+
+  axios.post('http://localhost:4000/todos/add', newTodo)
+    .then(res => console.log(res.data));
 
   // resetting the form
   this.setState({
